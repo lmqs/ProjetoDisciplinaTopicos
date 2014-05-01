6950853f29f4b731df3d9fa9df63e537413578d9
Luciana Queiroz <lucianaqueiroz098@hotmail.com>
2014-05-01 14:12:58 -0300

    integracao banco tete

--- TopicosProjetoAndroid/gen/br/com/projetoquestoesconcurso/R.java
+++ TopicosProjetoAndroid/gen/br/com/projetoquestoesconcurso/R.java
@@ -21,20 +21,34 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int chamar=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int info=0x7f020002;
-        public static final int informacoes=0x7f020003;
-        public static final int iniciar=0x7f020004;
-        public static final int maps=0x7f020005;
-        public static final int resultados=0x7f020006;
+        public static final int certo=0x7f020000;
+        public static final int chamar=0x7f020001;
+        public static final int errado=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int info=0x7f020004;
+        public static final int informacoes=0x7f020005;
+        public static final int iniciar=0x7f020006;
+        public static final int maps=0x7f020007;
+        public static final int prox=0x7f020008;
+        public static final int resultados=0x7f020009;
+        public static final int validar=0x7f02000a;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
-        public static final int btnInfor=0x7f080001;
-        public static final int btnIniciar=0x7f080002;
-        public static final int btnMaps=0x7f080000;
-        public static final int btnResultados=0x7f080003;
+        public static final int action_settings=0x7f08000e;
+        public static final int btnCerto=0x7f080009;
+        public static final int btnErrado=0x7f08000a;
+        public static final int btnInfor=0x7f08000b;
+        public static final int btnIniciar=0x7f08000c;
+        public static final int btnProx=0x7f080008;
+        public static final int btnResultados=0x7f08000d;
+        public static final int button1=0x7f080007;
+        public static final int radioAlternativas=0x7f080000;
+        public static final int resp1=0x7f080001;
+        public static final int resp2=0x7f080002;
+        public static final int resp3=0x7f080003;
+        public static final int resp4=0x7f080004;
+        public static final int resp5=0x7f080005;
+        public static final int txtQuestao=0x7f080006;
     }
     public static final class layout {
         public static final int activity_geracao_provas=0x7f030000;
@@ -51,6 +65,7 @@ public final class R {
         public static final int btnInciar=0x7f050004;
         public static final int hello_world=0x7f050002;
         public static final int lbltitulo=0x7f050003;
+        public static final int radioGrupo=0x7f050007;
         public static final int title_activity_geracao_provas=0x7f050005;
         public static final int title_activity_questoes=0x7f050006;
     }
--- TopicosProjetoAndroid/src/br/com/projetoquestoesconcurso/Questoes.java
+++ TopicosProjetoAndroid/src/br/com/projetoquestoesconcurso/Questoes.java
@@ -1,10 +1,19 @@
 package br.com.projetoquestoesconcurso;
 
-import br.com.projetoquestoesconcurso.R;
+import java.io.IOException;
+import java.util.ArrayList;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import br.com.projetoquestoesconcurso.conexao.Conexao;
+import br.com.projetoquestoesconcurso.model.Questao;
 
 public class Questoes extends Activity {
 
@@ -12,16 +21,47 @@ public class Questoes extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_questoes);
+		SQLiteDatabase db = openOrCreateDatabase("bd_concurso2",
+				Context.MODE_PRIVATE, null);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-	 	getMenuInflater().inflate(R.menu.questoes, menu);
+		getMenuInflater().inflate(R.menu.questoes, menu);
 		return true;
 	}
 
-	
-	
-	
+	public void iniciar(View view) {
+
+		Intent i;
+		// Pegando Todas as QUestões do SQLite e passando para a outra tela
+		ArrayList<Questao> questions = getQuestionSetFromDb();
+		Log.i("QUESTOES", "PASSOU PELO METODO DE PEGAR AS QUESTOES DO BANCO");
+		i = new Intent(this, GeracaoProvas.class);
+
+		Bundle bundle = new Bundle();
+		bundle.putSerializable("objeto", questions);
+		i.putExtras(bundle);
+
+		startActivity(i);
+	}
+
+	private ArrayList<Questao> getQuestionSetFromDb() throws Error {
+
+		Conexao myDbHelper = new Conexao(this);
+		try {
+			myDbHelper.createDataBase();
+		} catch (IOException ioe) {
+			throw new Error("Unable to create database");
+		}
+		try {
+			myDbHelper.openDataBase();
+		} catch (SQLException sqle) {
+			throw sqle;
+		}
+		ArrayList<Questao> questions = myDbHelper.getQuestionSet();
+		myDbHelper.close();
+		return questions;
+	}
 }
